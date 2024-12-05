02bcb588a7b9b692d43cf714eac045c8b877b1f3
Paul <fernandespaul548@gmail.com>
2024-12-05 23:39:59 +0100

    add class

---
+++ model/reponse.php
@@ -0,0 +1,27 @@
+<?php
+
+class Reponse {
+
+    private int $id;
+    private string $lib;
+
+    public function __construct(int $id, string $lib) {
+        $this->id = $id;
+        $this->lib = $lib;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+    public function getLib(): string {
+        return $this->lib;
+    }
+    public function setId(int $id): void {
+        $this->id = $id;
+    }
+
+    public function setLib(string $lib): void {
+        $this->lib = $lib;
+    }
+}
